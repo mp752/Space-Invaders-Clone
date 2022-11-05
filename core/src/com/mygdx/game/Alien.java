f95f4198b8d3d13a34196e6ac5574e6b0377f27d
marco <marco23p@live.com>
2022-11-05 11:48:05 -0400

    Changed Alien speed

---
+++ core/src/com/mygdx/game/Alien.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Alien {
+    public Vector2 position;
+    public Vector2 position_initial;
+    public Sprite sprite;
+    public Boolean Alive = true;
+    public Alien(Vector2 _position, Texture img, Color color){
+        position = _position;
+        position_initial = position;
+        sprite = new Sprite(img);
+        sprite.setColor(color);
+        sprite.setScale(3);
+    }
+    public void Draw(SpriteBatch batch){
+        sprite.setPosition(position.x, position.y);
+        sprite.draw(batch);
+    }
+
+}
